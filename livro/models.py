1be91041f5f1dfcd7becf1403912f4bcbc757cc2
Márcio Oliveira <marcio035@gmail.com>
2022-06-23 17:18:45 -0400

    Adicionado models Livros

--- livro/models.py
+++ livro/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from datetime import date
 
-# Create your models here.
+class Livros(models.Model):
+    nome = models.CharField(max_length = 100, null=True)
+    autor = models.CharField(max_length= 30, null=True)
+    co_autor = models.CharField(max_length= 30, blank=True, null=True)
+    data_cadastro = models.DateField(default=date.today)
+    emprestado = models.BooleanField(default=False)
+    nome_emprestado = models.CharField(max_length= 30, blank=True, null=True)
+    data_emprestimo = models.DateTimeField(blank=True, null=True)
+    data_devolucao = models.DateTimeField(blank=True, null=True)
+    tempo_duracao = models.DateField(blank=True, null=True)
+    
+    class Meta:
+        verbose_name = 'Livro'
+        
+    def __str__(self):
+        return self.nome
