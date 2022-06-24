4d3323541898bc7f309d462d315ecda2051fa1f9
Márcio Oliveira <marcio035@gmail.com>
2022-06-24 18:55:29 -0400

    adidionando validação de cadastro de usuário

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,6 +1,7 @@
-from multiprocessing.reduction import send_handle
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from hashlib import sha256
+from .models import Usuario
 
 def login(request):
     return HttpResponse('login')
@@ -13,4 +14,22 @@ def valida_cadastro(request):
     senha = request.POST.get('senha')
     email = request.POST.get('email')
     
-    return HttpResponse(f"{nome} {senha} {email} ")
+    usuario = Usuario.objects.filter(email = email)
+    
+    if len(nome.strip()) == 0 or len(email.strip()) == 0:
+        return redirect('/auth/cadastro/?status=1')
+    
+    if len(usuario) > 0:
+        return redirect('/auth/cadastro/?status=2')
+    
+    if len(senha) < 8:
+        return redirect('/auth/cadastro/?status=3')
+    
+    try:
+        senha = sha256(senha.encode()).hexdigest()
+        usuario = Usuario(nome = nome, email = email, senha = senha)
+        usuario.save()
+        return redirect('/auth/cadastro/?status=0')
+    
+    except:
+        return redirect('/auth/cadastro/?status=4')
