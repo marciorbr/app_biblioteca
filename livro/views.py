31f88bd9d89228e15ee02cca8c68fc8564e87cd6
Márcio Oliveira <marcio035@gmail.com>
2022-06-30 18:41:26 -0400

    Adicionando cards de lista de livro em home.html

--- livro/views.py
+++ livro/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect
 from usuarios.models import Usuario
+from livro.models import Livros
 
 def home(request):
     if request.session.get('usuario'):
         usuario = Usuario.objects.get(id = request.session['usuario'])
-        return render(request,'home.html')
+        livros = Livros.objects.filter( usuario = usuario )
+        return render(request,'home.html', {'livros': livros})
     else:
         return redirect('/auth/login/?status=2')
