86a18a5b89715a3d7d49fa08269a61a96e5234f2
Márcio Oliveira <marcio035@gmail.com>
2022-07-01 16:34:04 -0400

    adicionado url livro descricao_livro

--- livro/urls.py
+++ livro/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home, name = 'home')
+    path('home/', views.home, name = 'home'),
+    path('descricao_livro/<int:id>', views.descricao_livro, name = 'descricao_livro'),
 ]
