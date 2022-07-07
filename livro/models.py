a284f47d7e1a35c3cb1611c9de0b66663475d4ac
Márcio Oliveira <marcio035@gmail.com>
2022-07-07 17:19:47 -0400

    Adicionado na model livro class Emprestimos

--- livro/models.py
+++ livro/models.py
@@ -5,6 +5,7 @@ from usuarios.models import Usuario
 class Categoria(models.Model):
     nome = models.CharField(max_length=30)
     descricao = models.CharField(max_length=50)
+    usuario = models.ForeignKey(Usuario, on_delete=models.DO_NOTHING)
 
     def __str__(self) -> str:
         return self.nome
@@ -27,3 +28,14 @@ class Livros(models.Model):
         
     def __str__(self):
         return self.nome
+
+class Emprestimos(models.Model):
+    nome_emprestado = models.ForeignKey(Usuario, on_delete=models.DO_NOTHING, blank= True, null= True)
+    nome_emprestado_anonimo = models.CharField(max_length = 30, blank= True, null= True)
+    data_emprestimo = models.DateTimeField(blank= True, null= True)
+    data_devolucao = models.DateTimeField(blank= True, null= True)
+    tempo_duracao = models.DateTimeField(blank= True, null= True)
+    livro = models.ForeignKey(Livros, on_delete=models.DO_NOTHING)
+    
+    def __str__(self) -> str:
+        return self.nome_emprestado
