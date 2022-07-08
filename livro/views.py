e19741d36846d6f81d756511bba3f4ac2584bf4e
Márcio Oliveira <marcio035@gmail.com>
2022-07-08 17:30:30 -0400

    Inserindo restrição botão sair só para quando usuário estiver logado

--- livro/views.py
+++ livro/views.py
@@ -7,7 +7,8 @@ def home(request):
     if request.session.get('usuario'):
         usuario = Usuario.objects.get(id = request.session['usuario'])
         livros = Livros.objects.filter( usuario = usuario )
-        return render(request,'home.html', {'livros': livros})
+        usuario_logado = request.session.get('usuario')
+        return render(request,'home.html', {'livros': livros, 'usuario_logado': usuario_logado})
     else:
         return redirect('/auth/login/?status=2')
     
@@ -17,8 +18,9 @@ def descricao_livro(request, id):
         if request.session.get('usuario') == livros.usuario.id:
             categoria_livro = Categoria.objects.filter(usuario = request.session.get('usuario'))
             emprestimos = Emprestimos.objects.filter(livro = livros)
+            usuario_logado = request.session.get('usuario')
             return render(request, 'descricao_livro.html', {'livro': livros, 'categoria_livro': categoria_livro,
-                                                            'emprestimos': emprestimos })
+                                                            'emprestimos': emprestimos, 'usuario_logado': usuario_logado })
         else:
             return HttpResponse('Este livro não é seu')
     return redirect('/auth/login/?status=2')
