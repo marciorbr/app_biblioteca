6b34bf49f0478251237f4ebf2f5ca3b68702dd76
Márcio Oliveira <marcio035@gmail.com>
2022-07-08 15:03:34 -0400

    Removido tempo_duracao models livro

--- livro/models.py
+++ livro/models.py
@@ -34,5 +34,4 @@ class Emprestimos(models.Model):
     nome_emprestado_anonimo = models.CharField(max_length = 30, blank= True, null= True)
     data_emprestimo = models.DateField(blank= True, null= True)
     data_devolucao = models.DateField(blank= True, null= True)
-    tempo_duracao = models.DateField(blank= True, null= True)
     livro = models.ForeignKey(Livros, on_delete=models.DO_NOTHING)
