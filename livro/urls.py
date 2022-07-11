596b03bdb3d4a5aa9f98b17b3c20651aede79e43
Márcio Oliveira <marcio035@gmail.com>
2022-07-11 17:24:17 -0400

    Adicionando url cadastrar_livro

--- livro/urls.py
+++ livro/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('home/', views.home, name = 'home'),
     path('descricao_livro/<int:id>', views.descricao_livro, name = 'descricao_livro'),
+    path('cadastrar_livro/', views.cadastrar_livro, name='cadastrar_livro'),
 ]
