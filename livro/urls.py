4b22a2b5d016d619baf1437af6a56eb4ec893060
Márcio Oliveira <marcio035@gmail.com>
2022-07-13 14:52:31 -0400

    Adicionado url excluir_livro

--- livro/urls.py
+++ livro/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('home/', views.home, name = 'home'),
-    path('descricao_livro/<int:id>', views.descricao_livro, name = 'descricao_livro'),
+    path('descricao_livro/<int:id>', views.descricao_livro, name='descricao_livro'),
     path('cadastrar_livro/', views.cadastrar_livro, name='cadastrar_livro'),
+    path('excluir_livro/', views.excluir_livro, name='excluir_livro'),
 ]
