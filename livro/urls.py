8a1837b0a679f8171bfda714b1f39e291845e8b1
Márcio Oliveira <marcio035@gmail.com>
2022-07-13 14:54:06 -0400

    Alterado url excluir livro adicionando ao path id do livro

--- livro/urls.py
+++ livro/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('home/', views.home, name = 'home'),
     path('descricao_livro/<int:id>', views.descricao_livro, name='descricao_livro'),
     path('cadastrar_livro/', views.cadastrar_livro, name='cadastrar_livro'),
-    path('excluir_livro/', views.excluir_livro, name='excluir_livro'),
+    path('excluir_livro/<int:id>', views.excluir_livro, name='excluir_livro'),
 ]
